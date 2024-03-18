5e1c93914bab77021efd19520d4b0b5363a615fe
Shane-BR <beastiethepineapple@gmail.com>
2024-03-18 15:46:13 +1300

    Corrected PNGs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pyautogui
-import subprocess
 
+from sys import argv
+from subprocess import Popen
 from pathlib import Path
+
+# Allows the first arg to assign an amount of time to search for the buttons before failing
+button_search_time = int(argv[0]) if len(argv) > 0 and argv[0].isdigit else 15
 
 def main ():
     paths = open("paths.txt").read().split("\n")
@@ -9,25 +13,33 @@ def main ():
         paths[i] = paths[i].split(" ") # TODO remove
         try:
             print("Launching: " + paths[i][0])
-            process = subprocess.Popen([paths[i][0]])
+            process = Popen([paths[i][0]])
 
             filename = Path(paths[i][0]).stem
 
             navigateInstaller(filename)
-            # click all nececary buttons to begin instalation
-            # ALL PROGRAMS SHOULD BE INSTALLING AT THE SAME TIME
+
+            # wait until current program is finished then close it
+            # current then equals next opened program
+            # loop this until no more open programs remain
 
         except:
             print("Unable to launch: " + paths[i][0])
         
 
 def navigateInstaller(filename):
+    # click all nececary buttons to begin instalation
+    # ALL PROGRAMS SHOULD BE INSTALLING AT THE SAME TIME
+
     # find folder that is called "filename"
     # press each button png in step order
     step = 1
-    while button := pyautogui.locateOnScreen("resources/" + filename + "/" + str(step) + ".png", grayscale=False) != None:
+    path = "resources/" + filename + "/" + str(step) + ".PNG"
+    while button := pyautogui.locateOnScreen(path, minSearchTime=button_search_time) != None:
         pyautogui.click(button)
         step += 1
+    else:
+        print("Unable to locate button for step " + step +  "!")
     
 
 if __name__ == "__main__":

1e2d927a506ef19c234e37d4de9647f6d9cbdf2a
Shane-BR <beastiethepineapple@gmail.com>
2024-03-18 17:54:49 +1300

    + Change res to source image res

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pyautogui
+import win32api
+import win32con
+import pywintypes
 
 from sys import argv
 from subprocess import Popen
 from pathlib import Path
+from time import sleep
 
 # Allows the first arg to assign an amount of time to search for the buttons before failing
-button_search_time = int(argv[0]) if len(argv) > 0 and argv[0].isdigit else 15
+button_search_time = int(argv[1]) if len(argv) > 1 and argv[1].isdigit else 15
+op_width = int(argv[2]) if len(argv) > 2 and argv[2].isdigit else 1280
+op_height = int(argv[3]) if len(argv) > 3 and argv[3].isdigit else 1024
+
+init_width = win32api.GetSystemMetrics(0)
+init_height = win32api.GetSystemMetrics(1)
 
 def main ():
+
+    # change os resolution to fit the given screenshots
+    setWindowsResolution(op_width, op_height)
+
     paths = open("paths.txt").read().split("\n")
     for i in range(len(paths)):
         paths[i] = paths[i].split(" ") # TODO remove
@@ -25,7 +38,9 @@ def main ():
 
         except:
             print("Unable to launch: " + paths[i][0])
-        
+
+    # change resolution back to original
+    setWindowsResolution(init_width, init_width)   
 
 def navigateInstaller(filename):
     # click all nececary buttons to begin instalation
@@ -34,13 +49,19 @@ def navigateInstaller(filename):
     # find folder that is called "filename"
     # press each button png in step order
     step = 1
-    path = "resources/" + filename + "/" + str(step) + ".PNG"
-    while button := pyautogui.locateOnScreen(path, minSearchTime=button_search_time) != None:
-        pyautogui.click(button)
+    base_path = "resources/" + filename
+    while (button := pyautogui.locateOnScreen(base_path + "/" + str(step) + ".PNG", minSearchTime=button_search_time)) != None:
+        pyautogui.moveTo(button)
+        sleep(5)
         step += 1
-    else:
-        print("Unable to locate button for step " + step +  "!")
     
+def setWindowsResolution(width, height):
+    devmode = pywintypes.DEVMODEType()
+    devmode.PelsWidth = width
+    devmode.PelsHeight = height
+    devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
+
+    win32api.ChangeDisplaySettings(devmode, 0)
 
 if __name__ == "__main__":
     main()

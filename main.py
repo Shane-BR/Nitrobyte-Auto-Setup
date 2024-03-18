7b67ea84e0c3e9e01e5dc0dc26ddce692c0e0df9
Shane-BR <beastiethepineapple@gmail.com>
2024-03-18 18:49:16 +1300

    Programs should now close after installing (Awaiting testing)

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pyautogui
 import win32api
 import win32con
-import pywintypes
 
+from pywintypes import DEVMODEType
 from sys import argv
-from subprocess import Popen
+from subprocess import Popen, check_output
 from pathlib import Path
 from time import sleep
 
 # Allows the first arg to assign an amount of time to search for the buttons before failing
-button_search_time = int(argv[1]) if len(argv) > 1 and argv[1].isdigit else 15
+button_search_time = int(argv[1]) if len(argv) > 1 and argv[1].isdigit else 3
 op_width = int(argv[2]) if len(argv) > 2 and argv[2].isdigit else 1280
 op_height = int(argv[3]) if len(argv) > 3 and argv[3].isdigit else 1024
 
@@ -32,15 +32,33 @@ def main ():
 
             navigateInstaller(filename)
 
-            # wait until current program is finished then close it
-            # current then equals next opened program
-            # loop this until no more open programs remain
-
         except:
             print("Unable to launch: " + paths[i][0])
 
+    # wait until current program is finished then close it
+    # current then equals next opened program
+    # loop this until no more open programs remain
+
+    while True:
+        processes_running = 0
+        for i in range(len(paths)):
+            process = Path(paths[i][0]).name
+            if (isProcessRunning(process)):
+                # is there a finish button?
+                finish_button_img_path = "resources/" + process.split(".")[0] + "/finish.PNG"
+                if (button := pyautogui.locateOnScreen(finish_button_img_path, minSearchTime=button_search_time)) != None:
+                    pyautogui.click(button)
+                else:
+                    processes_running += 1
+
+        if processes_running == 0:
+            break
+
+
+
+
     # change resolution back to original
-    setWindowsResolution(init_width, init_width)   
+    setWindowsResolution(init_width, init_height)   
 
 def navigateInstaller(filename):
     # click all nececary buttons to begin instalation
@@ -56,12 +74,18 @@ def navigateInstaller(filename):
         step += 1
     
 def setWindowsResolution(width, height):
-    devmode = pywintypes.DEVMODEType()
+    devmode = DEVMODEType()
     devmode.PelsWidth = width
     devmode.PelsHeight = height
     devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
 
     win32api.ChangeDisplaySettings(devmode, 0)
+
+def isProcessRunning(process_name):
+    cmd = 'tasklist /fi "imagename eq {}"'.format(process_name)
+    out = check_output(cmd, shell=True).decode()
+
+    return process_name.lower() in out.lower()
 
 if __name__ == "__main__":
     main()

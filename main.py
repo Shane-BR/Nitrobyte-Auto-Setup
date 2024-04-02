b8afc29d351b53b143cf7b0178ee1b63bea1283d
Shane-BR <beastiethepineapple@gmail.com>
2024-04-02 23:04:41 +1300

    Changed to silent install rather than gui

--- main.py
+++ main.py
@@ -1,112 +1,63 @@
-import pyautogui
-import win32api
-import win32con
+import os
 
-from os import listdir, path
-from pywintypes import DEVMODEType
-from sys import argv
-from subprocess import Popen, check_output
+from subprocess import Popen
 from pathlib import Path
 from time import sleep
 
-# Allows the first arg to assign an amount of time to search for the buttons before failing
-button_search_time = int(argv[1]) if len(argv) > 1 and argv[1].isdigit else 60
-op_width = int(argv[2]) if len(argv) > 2 and argv[2].isdigit else 1280
-op_height = int(argv[3]) if len(argv) > 3 and argv[3].isdigit else 1024
-
-init_width = win32api.GetSystemMetrics(0)
-init_height = win32api.GetSystemMetrics(1)
-
 def main ():
 
-    # Turn off pyautogui exceptions
-    pyautogui.useImageNotFoundException(False)
+    os.system("") # needed for ANSI escape codes ¯\_(ツ)_/¯
+    paths = open("paths.txt").read().split("\n")
+    processes = []
+    for i in range(len(paths)):
 
-    # change os resolution to fit the given screenshots
-    setWindowsResolution(op_width, op_height)
+            if os.path.isfile(paths[i]) == False:
+                continue
 
-    try:
-        paths = open("paths.txt").read().split("\n")
-        for i in range(len(paths)):
-            try:
-                print("Launching: " + paths[i])
-                process = Popen([paths[i]])
-
-                filename = Path(paths[i]).stem
-
-                navigateInstaller(filename)
-
-            except Exception as exc:
-                print("An error occurred with process: " + paths[i] + ": " + str(exc))
-
-        # wait until current program is finished then close it
-        # current then equals next opened program
-        # loop this until no more open programs remain
-
-        while True:
-            processes_running = 0
-            for i in range(len(paths)):
-                process = Path(paths[i]).name
-                if (isProcessRunning(process)):
-                    # is there a finish button?
-                    finish_button_img_path = "resources/" + process.split(".")[0] + "/finish.PNG"
-                    if pyautogui.locateOnScreen(finish_button_img_path, minSearchTime=button_search_time) != None:
-                        Popen("taskkill /im " + process)
-                    else:
-                        processes_running += 1
-
-            if processes_running == 0:
-                break
-
-
-        # change resolution back to original
-        setWindowsResolution(init_width, init_height)   
-    except Exception as exc:
-        print("An error occurred with the main process: " + str(exc))
-        setWindowsResolution(init_width, init_height) 
-
-def navigateInstaller(filename):
-    # click all nececary buttons to begin instalation
-    # ALL PROGRAMS SHOULD BE INSTALLING AT THE SAME TIME
-
-    # find folder that is called "filename"
-    # press each button png in step order
-    step = 1
-    base_path = "resources/" + filename
-    while path.isfile(step_file := base_path + "/" + str(step) + ".PNG"):
-        
-        # Halt if any "wait" images are seen
-        skip = False
-        for file in listdir(base_path):
-            if (file.startswith("wait") and file.endswith(".PNG")):
-                wpath = base_path + "/" + file
-                if pyautogui.locateOnScreen(wpath) != None:
-                    skip = True
-                    break
-        
-        if (skip): 
-            continue
-
-        sleep(0.25)
-
-        if (button := pyautogui.locateOnScreen(step_file, minSearchTime=button_search_time)) != None:
-            pyautogui.click(button)
-            pyautogui.moveTo(op_width/2, 0)
-            step += 1
+            p = Popen("msiexec.exe /i {} /qn ".format(os.path.abspath(paths[i]))) if paths[i].endswith("msi") else Popen(paths[i] + " /S /s /silent /verysilent -qn -s /norestart")
+            processes.append(p)
     
-def setWindowsResolution(width, height):
-    devmode = DEVMODEType()
-    devmode.PelsWidth = width
-    devmode.PelsHeight = height
-    devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
+    # Loop through all processes and see if they are still running or if they are done
+    # check each 1/10 second
+    syms = "|/-\\"
+    loading_index = 0
+    while True:
+        cur_running = 0
 
-    win32api.ChangeDisplaySettings(devmode, 0)
+        fails = 0
 
-def isProcessRunning(process_name):
-    cmd = 'tasklist /fi "imagename eq {}"'.format(process_name)
-    out = check_output(cmd, shell=True).decode()
+        print("\033[H", end="")
+        print('\033[?25l', end="")
+        loading_index += 1
+        for process in processes:
+            process.poll()
+            loading_char = syms[loading_index % len(syms)]
 
-    return process_name.lower() in out.lower()
+            exec = process.args.split(" ")
+            name = os.path.basename(exec[2] if exec[0] == "msiexec.exe" else exec[0])
+
+            if process.returncode == None:
+                 print(name + " - \033[1m" + loading_char)
+                 cur_running += 1
+            elif process.returncode != 0:
+                 print(name + " - \033[1;31m ERR ({})".format(process.returncode))
+                 fails += 1
+            elif process.returncode == 0:
+                 print(name + " - \033[1;32m DONE")
+            
+            print("\033[0m", end="")
+        
+        if (cur_running <= 0):
+             break
+        
+        sleep(0.1)
+
+    print('\033[?25h', end="")
+
+    num_process = len(processes)
+    print("\nSuccessfully installed {}/{} programs.".format(num_process-fails,num_process))
+    os.system("pause")
+         
 
 if __name__ == "__main__":
     main()
